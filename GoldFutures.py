0e69ae3bd0118cfadcd16949ee7ed03b7abeffc3
Jeffrey-Adjei <118208940+Jeffrey-Adjei@users.noreply.github.com>
2025-04-07 14:01:10 +0100

    Abin & I will be doing some Linear Regression clear

---
+++ GoldFutures.py
@@ -0,0 +1,40 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5d6d13bb-ef60-4a17-8542-04efde84408a",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import numpy as np\n",
+    "import pandas as pd\n",
+    "import matplotlib.pyplot as plt\n",
+    "from scikit.linear_model import LinearRegression\n",
+    "from scikit.model_selection import train_test_split\n",
+    "from scikit.metrics import mean_squared_error, mean_absolute_error, r2_score"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
